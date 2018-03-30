96745f718e03a67b585a97f5b91da53e6ba8c7a3
Hammak <saint3543jakuzi@gmail.com>
2018-03-30 16:49:59 +0300

    Classes init

--- src/com/Hammak/Main.java
+++ src/com/Hammak/Main.java
@@ -3,6 +3,6 @@ package com.Hammak;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/com/Hammak/Pair.java
@@ -0,0 +1,4 @@
+package com.Hammak;
+
+public class Pair {
+}
