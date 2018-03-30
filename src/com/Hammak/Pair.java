fae7bb2f9053215527531491835d7d4b5ba8af6e
Hammak <saint3543jakuzi@gmail.com>
2018-03-30 17:05:56 +0300

    Create pair class fields

--- src/com/Hammak/Pair.java
+++ src/com/Hammak/Pair.java
@@ -1,4 +1,30 @@
 package com.Hammak;
 
+import java.time.LocalTime;
+
 public class Pair {
+    private boolean empty;
+
+    private int number;
+    private LocalTime startTime;
+    private String subject;
+    private char type;
+    private String teacher;
+    private int lectureHallNumber;
+
+    public Pair(int number, LocalTime startTime) {
+        this.empty = true;
+        this.number = number;
+        this.startTime = startTime;
+    }
+
+    public Pair(int number, LocalTime startTime, String subject, char type, String teacher, int lectureHallNumber) {
+        this.empty = false;
+        this.number = number;
+        this.startTime = startTime;
+        this.subject = subject;
+        this.type = type;
+        this.teacher = teacher;
+        this.lectureHallNumber = lectureHallNumber;
+    }
 }
