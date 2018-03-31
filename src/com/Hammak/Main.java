c5b373bc0723add8a5508530a3c99a7984d64de2
Hammak <saint3543jakuzi@gmail.com>
2018-03-31 16:50:20 +0300

    Init TableHandler with schedule table

--- src/com/Hammak/Main.java
+++ src/com/Hammak/Main.java
@@ -1,8 +1,19 @@
 package com.Hammak;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String CHARSET = "windows-1251";
 
+    private static final int TABLE_START_LINE = 8;
+    private static final int TABLE_END_LINE = 24;
+
+    public static void main(String[] args) throws IOException {
+        var tableHandler = new TableHandler(Files.readAllLines(Paths.get(args[0]),
+                Charset.forName(CHARSET)).subList(TABLE_START_LINE, TABLE_END_LINE));
     }
 }
--- src/com/Hammak/TableHandler.java
+++ src/com/Hammak/TableHandler.java
@@ -1,5 +1,11 @@
 package com.Hammak;
 
-public class tableHandler {
-    
+import java.util.List;
+
+public class TableHandler {
+    private List<String> lines;
+
+    public TableHandler(List<String> lines) {
+        this.lines = lines;
+    }
 }
