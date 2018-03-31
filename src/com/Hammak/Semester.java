5c231456817059b1bdce5592f58a3c6891f7bec1
Hammak <saint3543jakuzi@gmail.com>
2018-03-31 16:27:36 +0300

    Create semester class fields

--- src/com/Hammak/Semester.java
+++ src/com/Hammak/Semester.java
@@ -1,4 +1,30 @@
 package com.Hammak;
 
+import java.util.ArrayList;
+
 public class Semester {
+    private boolean empty;
+    private ArrayList<Week> weeks;
+
+    public Semester(ArrayList<Week> weeks) {
+        this.weeks = weeks;
+        this.empty = emptinessCheck();
+    }
+
+    private boolean emptinessCheck() {
+        for (Week week : weeks) {
+            if (!week.isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public ArrayList<Week> getWeeks() {
+        return weeks;
+    }
 }
