883925b590a2215a25938fa1239e06490f1a1736
Hammak <saint3543jakuzi@gmail.com>
2018-04-18 00:32:22 +0300

    Optimized SDS Class

--- src/com/Hammak/SomeDataStructure.java
+++ src/com/Hammak/SomeDataStructure.java
@@ -2,42 +2,36 @@ package com.Hammak;
 
 import java.time.LocalDate;
 
-public class SomeDataStructure {
+class SomeDataStructure {
     private int lectureHallNumber;
     private LocalDate startDay;
     private LocalDate endDay;
 
-    public SomeDataStructure() {
+    SomeDataStructure() {
 
     }
 
-    public SomeDataStructure(int lectureHallNumber, LocalDate startDay, LocalDate endDay) {
-        this.lectureHallNumber = lectureHallNumber;
-        this.startDay = startDay;
-        this.endDay = endDay;
-    }
-
-    public int getLectureHallNumber() {
+    int getLectureHallNumber() {
         return lectureHallNumber;
     }
 
-    public void setLectureHallNumber(int lectureHallNumber) {
+    void setLectureHallNumber(int lectureHallNumber) {
         this.lectureHallNumber = lectureHallNumber;
     }
 
-    public LocalDate getStartDay() {
+    LocalDate getStartDay() {
         return startDay;
     }
 
-    public void setStartDay(LocalDate startDay) {
+    void setStartDay(LocalDate startDay) {
         this.startDay = startDay;
     }
 
-    public LocalDate getEndDay() {
+    LocalDate getEndDay() {
         return endDay;
     }
 
-    public void setEndDay(LocalDate endDay) {
+    void setEndDay(LocalDate endDay) {
         this.endDay = endDay;
     }
 }
