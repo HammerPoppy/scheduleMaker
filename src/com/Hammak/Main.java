e3d8f623b5cce1b7a928aa69d5f377639afe6d11
Hammak <saint3543jakuzi@gmail.com>
2018-05-24 13:09:32 +0300

    Main extends Application

--- src/com/Hammak/Main.java
+++ src/com/Hammak/Main.java
@@ -1,11 +1,25 @@
 package com.Hammak;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) throws IOException {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("scheduleMaker");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
 }
