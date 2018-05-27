532ca0bc7807f96a9f0c63ef40d5f242964e451e
Hammak <saint3543jakuzi@gmail.com>
2018-05-27 16:00:58 +0300

    Core fields added

--- src/com/Hammak/FilePicker/FilePickerController.java
+++ src/com/Hammak/FilePicker/FilePickerController.java
@@ -1,4 +1,23 @@
 package com.hammak.FilePicker;
 
-public class FilePickerController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+
+import java.io.File;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+
+public class FilePickerController implements Initializable {
+    public GridPane gpList;
+    public ScrollPane spList;
+
+    private HashSet<File> fileList;
+    private boolean listIsEmpty = true;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        fileList = new HashSet<>();
+    }
 }
